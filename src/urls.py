d6666e2dacf03869fd9e3c479e644c70dff4118a
xudikk <71259765+xudoyberdi123@users.noreply.github.com>
2023-06-14 19:36:10 +0500

    generatror

--- src/urls.py
+++ src/urls.py
@@ -20,7 +20,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include("dashboard.urls")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
